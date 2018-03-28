7e40f9a1b26c0d40f0447f842e73c4f860f03e0e
Valerii <ganster1792@yandex.ru>
2018-03-28 19:02:18 +0400

    Главная(0.1)

---
+++ js/script.js
@@ -0,0 +1,22 @@
+  var link = document.querySelector(".search-hotel-btn");
+  var popup = document.querySelector(".search-wrapper");
+
+  popup.classList.add("search-wrapper-on");
+  link.addEventListener('click',function(event){event.preventDefault();
+
+    if (popup.classList.contains("search-wrapper-on")) {
+            popup.classList.add("search-wrapper-off");
+            popup.classList.remove('search-wrapper-on')
+  }
+
+    else if
+      (popup.classList.contains("search-wrapper-off"))
+      {popup.classList.add("search-wrapper-on");
+      popup.classList.remove("search-wrapper-off")
+    }})
+
+    ;window.addEventListener("keydown",function(event){
+      if(event.keyCode===27){
+        if(popup.classList.contains("search-wrapper-off")){
+          popup.classList.remove("search-wrapper-off")}
+    }});
