f8aab6abd27aa0ff5760dc796460980c0aad6778
Valerii <ganster1792@yandex.ru>
2018-04-01 00:55:17 +0400

    Главная(0.16)+Отели(0.25)

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
   var link = document.querySelector(".search-hotel-btn");
   var popup = document.querySelector(".search-wrapper");
 
-  popup.classList.add("search-wrapper-on");
+  popup.classList.add("search-wrapper-off-dis");
   link.addEventListener('click',function(event){event.preventDefault();
 
-    if (popup.classList.contains("search-wrapper-on")) {
+    if (popup.classList.contains("search-wrapper-off-dis")) {
+            popup.classList.add("search-wrapper-on");
+            popup.classList.remove("search-wrapper-off-dis")}
+
+    else if
+      (popup.classList.contains("search-wrapper-on")){
             popup.classList.add("search-wrapper-off");
             popup.classList.remove("search-wrapper-on");
             setTimeout("popup.classList.add('search-wrapper-off-dis')", 570)
-  }
-
-    else if
-      (popup.classList.contains("search-wrapper-off"))
-      {popup.classList.add("search-wrapper-on");
-      popup.classList.remove("search-wrapper-off");
-      popup.classList.remove("search-wrapper-off-dis");
     }})
 
     ;window.addEventListener("keydown",function(event){
       if(event.keyCode===27){
-        if(popup.classList.contains("search-wrapper-off")){
-          popup.classList.remove("search-wrapper-off")}
+        if(popup.classList.contains("search-wrapper-on")){
+            popup.classList.add("search-wrapper-off");
+            popup.classList.remove("search-wrapper-on");
+            setTimeout("popup.classList.add('search-wrapper-off-dis')", 570)}
     }});
