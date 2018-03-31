4af6dcc22056742307c8a1664771175305e66fdb
Valerii <ganster1792@yandex.ru>
2018-03-31 15:04:19 +0400

    Главная(0.13)+Отели(0.2)

--- js/script.js
+++ js/script.js
@@ -6,13 +6,15 @@
 
     if (popup.classList.contains("search-wrapper-on")) {
             popup.classList.add("search-wrapper-off");
-            popup.classList.remove("search-wrapper-on")
+            popup.classList.remove("search-wrapper-on");
+            setTimeout("popup.classList.add('search-wrapper-off-dis')", 570)
   }
 
     else if
       (popup.classList.contains("search-wrapper-off"))
       {popup.classList.add("search-wrapper-on");
-      popup.classList.remove("search-wrapper-off")
+      popup.classList.remove("search-wrapper-off");
+      popup.classList.remove("search-wrapper-off-dis");
     }})
 
     ;window.addEventListener("keydown",function(event){
